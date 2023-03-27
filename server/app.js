64c8eda3ea9ed54d84799217c0d62662cc4cc6ea
trungle2910 <thanhtrungle290197@gmail.com>
2023-03-27 17:43:56 +0700

    connect Postgre and made data for drink

--- server/app.js
+++ server/app.js
@@ -5,6 +5,7 @@ var express = require('express');
 var path = require('path');
 var cookieParser = require('cookie-parser');
 var logger = require('morgan');
+const {Pool} = require("pg");
 
 var indexRouter = require('./routes/index');
 
@@ -17,6 +18,26 @@ app.use(cookieParser());
 app.use(express.static(path.join(__dirname, 'public')));
 
 app.use(cors());
-app.use('/', indexRouter);
+app.use('/api', indexRouter);
+
+const pool = new Pool({
+    user: 'postgres',
+    host: 'localhost',
+    database: 'Coffe_House',
+    password: `${process.env.PGPASS}`,
+    port: 5432,
+  });
+// Test kết nối
+pool.query('SELECT NOW()', (err, res) => {
+    if (err) {
+      console.error('Error connecting to the database', err.stack);
+    } else {
+      console.log('Connected to database at', res.rows[0].now);
+    }
+  });
+
+  
+  pool.query(`CREATE SCHEMA IF NOT EXISTS drinks_cf`);
+
 
 module.exports = app;
--- server/routes/index.js
+++ server/routes/index.js
@@ -1,13 +1,14 @@
 var express = require('express');
 var router = express.Router();
 
+
 /* GET home page. */
-router.get('/', function(req, res, next) {
-  const  data ={
-    name: "Trung",
-    action: "Hello world"
-  }
-  res.status(200).json(data);
-});
+// router.get('/', function(req, res, next) {
+//   res.send("hello");
+// });
+
+// get drink page
+const drinkAPI = require("./drink.api.js")
+router.use("/drink", drinkAPI)
 
 module.exports = router;
